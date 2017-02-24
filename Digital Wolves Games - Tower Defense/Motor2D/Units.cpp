31971e06eb8f649253b6f90baeb1cc6eb7970a36
martimyc <martimyc2@gmail.com>
2017-02-24 19:16:40 +0100

    lil funct for text testing: A- attack S- Die D- disappear W- walk I- idle

--- Digital Wolves Games - Tower Defense/Motor2D/Units.cpp
+++ Digital Wolves Games - Tower Defense/Motor2D/Units.cpp
@@ -1,7 +1,10 @@
+
 #include "j1App.h"
+#include "j1Input.h"
 #include "j1Render.h"
 #include "j1Animation.h"
 #include "Units.h"
+
 
 Unit::Unit(UNIT_TYPE u_type, iPoint pos): Entity(UNIT, pos), unit_type(u_type), direction(EAST), action_type(IDLE)
 {}
@@ -19,6 +22,20 @@ void Unit::Move()
 
 void Unit::AI()
 {
+	if (App->input->GetKey(SDL_SCANCODE_A) == KEY_DOWN)
+		action_type = ATTACK;
+
+	if (App->input->GetKey(SDL_SCANCODE_I) == KEY_DOWN)
+		action_type = IDLE;
+
+	if (App->input->GetKey(SDL_SCANCODE_W) == KEY_DOWN)
+		action_type = WALK;
+
+	if (App->input->GetKey(SDL_SCANCODE_D) == KEY_DOWN)
+		action_type = DISAPPEAR;
+
+	if (App->input->GetKey(SDL_SCANCODE_S) == KEY_DOWN)
+		action_type = DIE;
 }
 
 void Unit::Draw()
